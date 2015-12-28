2a7f4f588baab70e6d325993c230f7b4eda52d73
donovan6000 <donovancudney@yahoo.com>
2015-12-27 22:40:28 -0800

    OS X installer adds python location to PATH so that OctoPrint can locate pip on its own

--- installers/OS X/install.sh
+++ installers/OS X/install.sh
@@ -28,7 +28,8 @@ else
 		curl -o python.pkg https://www.python.org/ftp/python/${version}/python-${version}-macosx10.6.pkg
 		installer -pkg python.pkg -target /
 		rm python.pkg
-		pythonVersion="$(cut -f1,2 -d'.' <<< ${version})"
+		echo 'export PATH=/Library/Frameworks/Python.framework/Versions/'"$(cut -f1,2 -d'.' <<< ${version})"'/bin:$PATH' >> '/Users/'"$SUDO_USER"'/.bash_profile'
+		source '/Users/'"$SUDO_USER"'/.bash_profile'
 
 		# Install command line tools
 		curl -O 'https://raw.githubusercontent.com/donovan6000/M3D-Fio/master/installers/OS%20X/command%20line%20tools%20installer.bash'
@@ -51,7 +52,7 @@ else
                 except subprocess.CalledProcessError as e:\
                     self.sdk_root = \'"'"'\/\'"'"'/g' setup.py
 
-		/Library/Frameworks/Python.framework/Versions/${pythonVersion}/bin/python setup.py install
+		python setup.py install
 		cd ..
 		rm -rf pyobjc-core-${version}
 
@@ -63,7 +64,7 @@ else
 		tar zxvf pyobjc-framework-Cocoa.tar.gz
 		rm pyobjc-framework-Cocoa.tar.gz
 		cd pyobjc-framework-Cocoa-${version}
-		/Library/Frameworks/Python.framework/Versions/${pythonVersion}/bin/python setup.py install
+		python setup.py install
 		cd ..
 		rm -rf pyobjc-framework-Cocoa-${version}
 		
@@ -71,15 +72,15 @@ else
 		curl -LOk https://github.com/foosel/OctoPrint/archive/master.zip
 		unzip master.zip
 		cd OctoPrint-master
-		/Library/Frameworks/Python.framework/Versions/${pythonVersion}/bin/python setup.py install
+		python setup.py install
 		cd ..
 		rm -rf OctoPrint-master
 		rm master.zip
 
 		# Install M3D Fio
-		echo 'y' | /Library/Frameworks/Python.framework/Versions/${pythonVersion}/bin/pip uninstall OctoPrint-M3DFio
+		echo 'y' | pip uninstall OctoPrint-M3DFio
 		curl -LOk https://github.com/donovan6000/M3D-Fio/archive/master.zip
-		while ! /Library/Frameworks/Python.framework/Versions/${pythonVersion}/bin/pip install master.zip
+		while ! pip install master.zip
 		do
 			:
 		done
@@ -87,7 +88,7 @@ else
 		
 		# Add OctoPrint to startup programs
 		curl -O 'https://raw.githubusercontent.com/donovan6000/M3D-Fio/master/installers/OS%20X/com.octoprint.app.plist'
-		sed -i '' -e 's/\/path\/to\/octoprint/\/Library\/Frameworks\/Python.framework\/Versions\/'"${pythonVersion}"'\/bin\/octoprint/g' com.octoprint.app.plist
+		sed -i '' -e 's#/path/to/octoprint#'"$(type octoprint | cut -d' ' -f3)"'#g' com.octoprint.app.plist
 		mv com.octoprint.app.plist '/Library/LaunchAgents'
 
 		# Create URL link on desktop
